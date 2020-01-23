64b29ecb48bc6124e0c8878910176802df5ccec5
isaty <66satyamsingh@gmail.com>
2020-01-23 18:28:24 +0530

    view integrate

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const app=express()
 const path=require('path')
 // const public=path.join(__dirname,'public')
 const routes=require('./routes/route')
+const hbs=require('hbs')
 
 //connect to  database 
 require('./db/connect')
 const public=path.join(__dirname,'public')
+const view =patth.join(__dirname,'views')
 app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json({ extended: false }))
 //serving static files
 app.use(express.static(public))
-
+ app.set('view engine','hbs')
+ app.set('views',views)
 
 app.use(express.static(public))
 
